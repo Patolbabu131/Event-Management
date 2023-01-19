f4564662bb666dee881ac4c4a1044dfcc9fed569
Kevin <avishkaritservices1010@gmail.com>
2023-01-19 15:11:32 +0530

    Added the Basic Views and Controllers

---
+++ Events.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Events.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Events.Web/Controllers/ExecutiveMembersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Events.Web.Controllers
+{
+    public class ExecutiveMembersController : Controller
+    {
+        private readonly ILogger<ExecutiveMembersController> _logger;
+
+        public ExecutiveMembersController(ILogger<ExecutiveMembersController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
